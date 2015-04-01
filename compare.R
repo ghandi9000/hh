76c9d2022639c47ca0c337c7ff9146aefa640acd
Noah Peart <noah.v.peart@gmail.com>
2015-04-01 19:36:58 -0400

    Batch update

--- compare.R
+++ compare.R
@@ -3,7 +3,7 @@
 ## Description: Compare fits
 ## Author: Noah Peart
 ## Created: Mon Mar 30 18:04:51 2015 (-0400)
-## Last-Updated: Mon Mar 30 18:50:27 2015 (-0400)
+## Last-Updated: Wed Apr  1 19:22:46 2015 (-0400)
 ##           By: Noah Peart
 ######################################################################
 source("~/work/ecodatascripts/vars/heights/prep.R")                 # data prep
@@ -12,25 +12,30 @@ library(dplyr)
 library(magrittr)
 
 ## Re-run fast fits
+models <- c("gompertz", "power")
 inds <- c("full", "can", "elev")  # independent variable groupings
-spec <- "abba"
+specs <- c("abba", "beco")
 basedir <- paste0("~/work/hh/")
 can_func <- "can_hh"
 yrs <- c(99, 11)
 
 fits <- list()
-for (ind in inds) {
-    moddir <- paste0(basedir, ind, "/")
-    source(paste0(moddir, "model.R"))  # get model and fit function
-    for (yr in yrs) {
-        dat <- prep_hh(dat=tp, yr=yr, spec=spec, can_func=can_func)
-        ps <- readRDS(paste0(moddir, tolower(spec), "/", tolower(spec), "_", yr, ".rds"))
-        fit <- run_fit(dat, ps, yr)
-        name <- paste0(tolower(spec), yr, ind)
-        fits[name] <- fit
+for (model in models) {
+    moddir <- paste0(basedir, model, "/")
+    for (ind in inds) {
+        inddir <- paste0(moddir, ind, "/")
+        source(paste0(inddir, "model.R"))  # get model and fit function
+        for (spec in specs) {
+            for (yr in yrs) {
+                dat <- prep_hh(dat=tp, yr=yr, spec=spec, can_func=can_func)
+                ps <- readRDS(paste0(inddir, tolower(spec), "/", tolower(spec), "_", yr, ".rds"))
+                fit <- run_fit(dat, ps, yr)
+                name <- paste0(tolower(model),tolower(spec), yr, ind)
+                fits[name] <- fit
+            }
+        }
     }
 }
-
 ## Compare AICs
 aics <- lapply(fits, FUN=function(x) AICc(x, nobs = nrow(dat)))
 
